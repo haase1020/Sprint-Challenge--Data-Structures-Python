26d286073d036cdfdc632ae3e63907cc1b38df02
Jon Haase <haase1020@gmail.com>
2020-08-14 15:45:20 -0400

    update files for CS 34

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,3 +1,56 @@
+# class Node:
+#     def __init__(self, value=None, next_node=None):
+#         self.value = value
+#         self.next_node = next_node
+
+#     def get_value(self):
+#         return self.value
+
+#     def get_next(self):
+#         return self.next_node
+
+#     def set_next(self, new_next):
+#         self.next_node = new_next
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def add_to_head(self, value):
+#         node = Node(value)
+
+#         if self.head is not None:
+#             node.set_next(self.head)
+
+#         self.head = node
+
+#     def contains(self, value):
+#         if not self.head:
+#             return False
+
+#         current = self.head
+
+#         while current:
+#             if current.get_value() == value:
+#                 return True
+
+#             current = current.get_next()
+
+#         return False
+
+#     def reverse_list(self, node, prev):
+#         if node is None:
+#             return
+
+#         if node.get_next() == None:
+#             self.head = node
+#             self.head.next_node = prev
+#             return
+#         next = node.get_next()
+#         node.next_node = prev
+
+#         self.reverse_list(next, node)
 class Node:
     def __init__(self, value=None, next_node=None):
         self.value = value
@@ -39,15 +92,14 @@ class LinkedList:
 
         return False
 
-    def reverse_list(self, node, prev):
+    def reverse_list(self, node,  prev):
+        # Nothing changes in empty or single lists
         if node is None:
             return
 
-        if node.get_next() == None:
+        if node.next_node is None:
             self.head = node
             self.head.next_node = prev
-            return
-        next = node.get_next()
-        node.next_node = prev
-
-        self.reverse_list(next, node)
+        else:
+            self.reverse_list(node.next_node, node)
+            node.next_node = prev
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,30 +1,48 @@
 from doubly_linked_list import DoublyLinkedList
 
 
+# class RingBuffer:
+# def __init__(self, capacity):
+#     self.capacity = capacity
+#     self.current = None
+#     self.storage = DoublyLinkedList()
+
+# def append(self, item):
+#     # check to see if the list is completely full
+#     if len(self.storage) < self.capacity:
+#         self.storage.add_to_tail(item)
+#         self.current = self.storage.head
+#     else:
+#         if not self.current.next:
+#             self.current.value = item
+#             self.current = self.storage.head
+#         else:
+#             self.current.value = item
+#             self.current = self.current.next
+
+# def get(self):
+#     ring_buffer_content = []
+
+#     current = self.storage.head
+#     while current:
+#         ring_buffer_content.append(current.value)
+#         current = current.next
+#     return ring_buffer_content
+
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
-        self.storage = DoublyLinkedList()
+        self.current = 0
+        self.storage = []
 
     def append(self, item):
-        # check to see if the list is completely full
-        if len(self.storage) < self.capacity:
-            self.storage.add_to_tail(item)
-            self.current = self.storage.head
-        else:
-            if not self.current.next:
-                self.current.value = item
-                self.current = self.storage.head
-            else:
-                self.current.value = item
-                self.current = self.current.next
+        self.storage[self.current] = item
+        self.current += 1
+        # If we reach the capacity of the buffer
+        # set the current pointer back to the beginning
+        if self.current == self.capacity:
+            self.current = 0
 
     def get(self):
-        ring_buffer_content = []
-
-        current = self.storage.head
-        while current:
-            ring_buffer_content.append(current.value)
-            current = current.next
-        return ring_buffer_content
+        return [x for x in self.storage if x is not None]
